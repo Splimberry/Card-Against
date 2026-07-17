65d962a5c366283fd5b0c13b650c4dfdc083d831
Splimberry <Splimberry@users.noreply.github.com>
2026-07-17 14:18:46 +0800

    Add search metadata and sitemap

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".txt": "text/plain; charset=utf-8",
   ".md": "text/markdown; charset=utf-8",
   ".mp3": "audio/mpeg",
